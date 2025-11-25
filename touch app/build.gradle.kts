b52437da35cf69897fd8f22f0b35be6b07dc5dfc
cyberm-del <thabang.mashigo@gcon.ac.za>
2025-11-25 14:24:58 +0200

    Add files via upload

--- touch app/build.gradle.kts
+++ touch app/build.gradle.kts
@@ -1,1 +1,16 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("com.lagradost.cloudstream3.plugin") version "1.0.0" apply false
+}
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+    dependencies {
+        classpath("com.lagradost.cloudstream3:gradle-plugin:1.0.0")
+    }
+}
