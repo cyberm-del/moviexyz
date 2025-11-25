4798ae4a9594646791efd06e3fc7cd73046e38a8
cyberm-del <thabang.mashigo@gcon.ac.za>
2025-11-25 13:57:32 +0200

    Add files via upload

--- touch app/src/main/kotlin/com/moviexyz/multisource/MultiSourcePlugin.kt
+++ touch app/src/main/kotlin/com/moviexyz/multisource/MultiSourcePlugin.kt
@@ -1,1 +1,16 @@
+package com.moviexyz.multisource
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@Plugin
+class MultiSourcePlugin : BasePlugin() {
+    override fun load(context: Context) {
+        // Register all providers
+        registerMainAPI(MusicHQProvider())
+        registerMainAPI(MovieMazeProvider())
+        registerMainAPI(KickassAnimeProvider())
+        registerMainAPI(MirrorSiteProvider())
+    }
+}
